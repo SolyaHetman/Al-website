f51005ff7232e73cdace70394376c34fab066af6
SolyaHetman <solyahteman2013@gmail.com>
2017-12-27 00:47:30 +0200

    code refactoring

--- js/truncated.js
+++ js/truncated.js
@@ -10,7 +10,7 @@ let e1 = document.getElementsByClassName('truncated')[0],
     e10 = document.getElementsByClassName('truncated')[9],
     e11 = document.getElementsByClassName('truncated')[10],
     e12 = document.getElementsByClassName('truncated')[11],
-    length = 96,
+    length = 400,
     t1 = e1.innerHTML.substring(0, length);
 t2 = e2.innerHTML.substring(0, length);
 t3 = e3.innerHTML.substring(0, length);
@@ -34,7 +34,7 @@ e1.addEventListener("mouseout", () => {
 e1.addEventListener("mouseover", () => {
     let length = 98;
     let trimmedString = e1.innerHTML.substring(0, length);
-    document.getElementsByClassName('truncated')[0].innerHTML = t1 + ', tuesday, etc.) the bot now can learn - if you want to teach him some new phrases, just use a special command and he\'ll remember this (in the near future we will make this learning intelligent: the bot won\'t accept rude words and filter expressions)';
+    document.getElementsByClassName('truncated')[0].innerHTML = t1 + 'The main aim of the project is to teach the bot and give it artificial “brains”, which will make him as close to human as it’s possible.)';
 
 });
 
@@ -60,5 +60,54 @@ e3.addEventListener("mouseout", () => {
 e3.addEventListener("mouseover", () => {
     let length = 98;
     let trimmedString = e3.innerHTML.substring(0, length);
-    document.getElementsByClassName('truncated')[2].innerHTML = t3 + ', tuesday, etc.) the bot now can learn - if you want to teach him some new phrases, just use a special command and he\'ll remember this (in the near future we will make this learning intelligent: the bot won\'t accept rude words and filter expressions)';
+    document.getElementsByClassName('truncated')[2].innerHTML = t3 + ', All of us can see how nowadays generation slowly destroy nature and year after years the problem with poluting environment will get worse.So that ,we decided to do an imagination of the last plant in a few centuries, which will be the last source of oxygen in our world.)';
+});
+
+e4.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e4.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[3].innerHTML = t4;
+});
+
+e4.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e4.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[3].innerHTML = t4 + 'For many years we have been improving thetechnology, designing smart cars and building modern cities.But many people do not care about the environment and “kill” our planet. The result is global warming and climate change in recent years,A character from our game lives in an era in which mankind has completely exhausted the resources of our planet.  People were forced to move to other more fitting planets. But everyone was not able to move. Among the remaining people was our character. They are now forced to fight for survival on a polluted, abandoned planet every day.We hope that this game will make everyone think about our nature before it is too late.A special atmosphere and storyline in this game will not leave you indifferent.)';
+});
+
+e5.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e4.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[4].innerHTML = t5;
+});
+
+e5.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e4.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[4].innerHTML = t5 + 'which should jump as hight as it can to beat the highscore.Player could avoid falling barrels by jumping forward or backwards.If the character falls down,  the game restarts. We realized only small part of our plans,  because we started from a complete scratch with no knowledge in game dev sphere.  Due to this project we greatly improved our programming and designing skills and gained great teamwork expirience.    )';
+});
+
+e6.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e4.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[5].innerHTML = t6;
+});
+
+e6.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e4.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[5].innerHTML = t6 + 'We are planning to create few levels with different riddles and puzzles.)';
+});
+
+
+e7.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e4.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[6].innerHTML = t7;
+});
+
+e7.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e4.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[6].innerHTML = t7 + 'see other members and ratings among users. In addition, you can create your own events and invite people. You may send invitations to your friends and wait for their acceptance or refusal or may make the event available for everyone.)';
 });
