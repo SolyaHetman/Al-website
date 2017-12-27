af157389c402e44176dee2b4b0d96926bd79d1bc
SolyaHetman <solyahteman2013@gmail.com>
2017-12-27 09:14:43 +0200

    commit

--- js/truncated.js
+++ js/truncated.js
@@ -10,6 +10,17 @@ let e1 = document.getElementsByClassName('truncated')[0],
     e10 = document.getElementsByClassName('truncated')[9],
     e11 = document.getElementsByClassName('truncated')[10],
     e12 = document.getElementsByClassName('truncated')[11],
+    e13 = document.getElementsByClassName('truncated')[12],
+    e14 = document.getElementsByClassName('truncated')[13],
+    e15 = document.getElementsByClassName('truncated')[14],
+    e16 = document.getElementsByClassName('truncated')[15],
+    e17 = document.getElementsByClassName('truncated')[16],
+    e18 = document.getElementsByClassName('truncated')[17],
+    e19 = document.getElementsByClassName('truncated')[18],
+    e20 = document.getElementsByClassName('truncated')[19],
+    e23 = document.getElementsByClassName('truncated')[22],
+    e24 = document.getElementsByClassName('truncated')[23],
+    e25 = document.getElementsByClassName('truncated')[24],
     length = 400,
     t1 = e1.innerHTML.substring(0, length);
 t2 = e2.innerHTML.substring(0, length);
@@ -23,6 +34,17 @@ t9 = e9.innerHTML.substring(0, length);
 t10 = e10.innerHTML.substring(0, length);
 t11 = e11.innerHTML.substring(0, length);
 t12 = e12.innerHTML.substring(0, length);
+t13 = e13.innerHTML.substring(0, length);
+t14 = e14.innerHTML.substring(0, length);
+t15 = e15.innerHTML.substring(0, length);
+t16 = e16.innerHTML.substring(0, length);
+t17 = e17.innerHTML.substring(0, length);
+t18 = e18.innerHTML.substring(0, length);
+t19 = e19.innerHTML.substring(0, length);
+t20 = e20.innerHTML.substring(0, length);
+t23 = e23.innerHTML.substring(0, length);
+t24 = e24.innerHTML.substring(0, length);
+t25 = e25.innerHTML.substring(0, length);
 document.getElementsByClassName('truncated')[0].innerHTML = t1 + '...';
 
 e1.addEventListener("mouseout", () => {
@@ -77,32 +99,32 @@ e4.addEventListener("mouseover", () => {
 
 e5.addEventListener("mouseout", () => {
     let length = 100;
-    let trimmedString = e4.innerHTML.substring(0, length);
+    let trimmedString = e5.innerHTML.substring(0, length);
     document.getElementsByClassName('truncated')[4].innerHTML = t5;
 });
 
 e5.addEventListener("mouseover", () => {
     let length = 98;
-    let trimmedString = e4.innerHTML.substring(0, length);
+    let trimmedString = e5.innerHTML.substring(0, length);
     document.getElementsByClassName('truncated')[4].innerHTML = t5 + 'which should jump as hight as it can to beat the highscore.Player could avoid falling barrels by jumping forward or backwards.If the character falls down,  the game restarts. We realized only small part of our plans,  because we started from a complete scratch with no knowledge in game dev sphere.  Due to this project we greatly improved our programming and designing skills and gained great teamwork expirience.    )';
 });
 
 e6.addEventListener("mouseout", () => {
     let length = 100;
-    let trimmedString = e4.innerHTML.substring(0, length);
+    let trimmedString = e6.innerHTML.substring(0, length);
     document.getElementsByClassName('truncated')[5].innerHTML = t6;
 });
 
 e6.addEventListener("mouseover", () => {
     let length = 98;
-    let trimmedString = e4.innerHTML.substring(0, length);
+    let trimmedString = e6.innerHTML.substring(0, length);
     document.getElementsByClassName('truncated')[5].innerHTML = t6 + 'We are planning to create few levels with different riddles and puzzles.)';
 });
 
 
 e7.addEventListener("mouseout", () => {
     let length = 100;
-    let trimmedString = e4.innerHTML.substring(0, length);
+    let trimmedString = e7.innerHTML.substring(0, length);
     document.getElementsByClassName('truncated')[6].innerHTML = t7;
 });
 
@@ -111,3 +133,117 @@ e7.addEventListener("mouseover", () => {
     let trimmedString = e4.innerHTML.substring(0, length);
     document.getElementsByClassName('truncated')[6].innerHTML = t7 + 'see other members and ratings among users. In addition, you can create your own events and invite people. You may send invitations to your friends and wait for their acceptance or refusal or may make the event available for everyone.)';
 });
+
+
+e8.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e7.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[7].innerHTML = t8;
+});
+
+e8.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e8.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[7].innerHTML = t8 + 'I do not have this book! " "I wrote important information on a piece of paper, but I lost it! What could I do now??" Іf you are a student - you definitly understand what this sentences about. So we decided to develop an Android App which will solve such problems.)';
+})
+
+e10.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e10.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[9].innerHTML = t10;
+});
+
+e10.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e10.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[9].innerHTML = t10 + 'I do not have this book! " "I wrote important information on a piece of paper, but I lost it! What could I do now??" Іf you are a student - you definitly understand what this sentences about. So we decided to develop an Android App which will solve such problems.)';
+})
+
+
+e11.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e11.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[10].innerHTML = t11;
+});
+
+e11.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e11.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[10].innerHTML = t11 + ' The alarm clock will help get up, and the Attendance App will save time for learning something more during the lessonYou only need to take a student card and attach it to the camera. The application will scan your data and send them to the online list, that will be seen by the teacher.)';
+})
+
+e12.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e12.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[11].innerHTML = t12;
+});
+
+e12.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e12.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[11].innerHTML = t12 + ' Also you can find here the main map of location all university buildings and their photos,description,etc.Its show the right way between buildings of the university,transport which u need and,the location of the main transport stops.)';
+})
+
+
+e15.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e15.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[14].innerHTML = t15;
+});
+
+e15.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e15.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[14].innerHTML = t15 + '  The idea is to make a beautiful addition to the interior that could play interesting patterns. Its size is 8 × 8 × 8. In the future on our 3D screen you will be able to portray many interesting implementations.)';
+})
+
+
+e18.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e15.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[17].innerHTML = t18;
+});
+
+e18.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e15.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[17].innerHTML = t18 + ' It is equipped with two sensors : ultrasound and colors sensors. Also robot has 2 standard wheels and 1 sphere wheel . We created such algorithm which gives robot the ability to find safe and correct way to get from one point to another.   )';
+})
+
+
+e23.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e23.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[22].innerHTML = t23;
+});
+
+e23.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e23.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[22].innerHTML = t23 + ' to the server with data from sensors. At this stage, we connected the ESP8266 and created an access point for WiFi to control the periphery (temperature sensors and humidity sensors).)';
+})
+
+
+e24.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e24.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[23].innerHTML = t24;
+});
+
+e24.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e24.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[23].innerHTML = t24 + ' Now, after thorough training process, we are working on the main part of the project – implementation of the idea. Our robot already can measure the distance, give light signals of the results, switch between different ways of executing of an order, make an output on display etc., and that’s only the beginning! It’s hard to imagine what will be next.)';
+})
+
+e25.addEventListener("mouseout", () => {
+    let length = 100;
+    let trimmedString = e25.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[24].innerHTML = t25;
+});
+
+e25.addEventListener("mouseover", () => {
+    let length = 98;
+    let trimmedString = e25.innerHTML.substring(0, length);
+    document.getElementsByClassName('truncated')[24].innerHTML = t25 + ' Each mode gives the user access only to his permitted goals, the time of use of restrictions, the user determines itself in the countdown timer. Promi gives access to the object that is to be controlled. How does the application work? Proxy provides access to objects that should be monitored and add functionality. And access control to the Internet is due to the use of Proxy Protection)';
+})
